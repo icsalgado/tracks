06e864f8213fdd0550a9d9bb811057b5f632224c
icsalgado <icsalgado@icloud.com>
2022-12-15 19:33:00 -0200

    TW-FormacaoJava - Saídas

--- TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
+++ TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
@@ -4,7 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá mundo");
+		System.out.println("Olá mundo"); // quebra a linha
+		System.out.print("Olá de novo"); // não quebra a linha
+		System.out.print("Olá mais uma vez \n"); // quebra a linha com
+													// metacaractere pode usar
+													// também o \r no windows
 	}
 
 }
