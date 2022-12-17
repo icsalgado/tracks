7f591d53305af595fa3c57c3d01a4c0e776dcb26
icsalgado <icsalgado@icloud.com>
2022-12-17 16:32:52 -0200

    DIO-FormacaoJavaDeveloper - padrão de variaveis e metodos - Anatomia das classes 3

--- DIO_FormacaoJavaDeveloper/java-anatomia-classes/src/MinhaClasse.java
+++ DIO_FormacaoJavaDeveloper/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
     public static void main(String [] args){ //metodo main recebe parametro args
-        System.out.print ("Primeira classe com metodo executavel");
+        //System.out.print ("Primeira classe com metodo executavel");
+
+        String primeiroNome = "Iuri";
+        String ultimoNome = "Salgado";
+
+        String nomeCompleto = completarNome (primeiroNome, ultimoNome);
+
+        System.out.print(nomeCompleto);
+    }
+
+    public static String completarNome (String primeiroNome, String ultimoNome){
+        return primeiroNome.concat(" ").concat(ultimoNome);
     }
 }
