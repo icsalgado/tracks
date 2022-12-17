fe02664696c46179d08cb9da3561eae46aa6e720
icsalgado <icsalgado@icloud.com>
2022-12-17 17:18:13 -0200

    DIO-FormacaoJavaDeveloper - padrão de criação de packages no vscode - Anatomia de classes 5

--- DIO_FormacaoJavaDeveloper/java-anatomia-classes/src/edu/iuri/aula/MinhaClasse.java
+++ DIO_FormacaoJavaDeveloper/java-anatomia-classes/src/edu/iuri/aula/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.iuri.aula;
 public class MinhaClasse {
     public static void main(String [] args){ //metodo main recebe parametro args
         //System.out.print ("Primeira classe com metodo executavel");
