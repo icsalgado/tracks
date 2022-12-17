e85757ddf84cf0cae0eca53103352f5d982fe64d
icsalgado <icsalgado@icloud.com>
2022-12-17 15:56:58 -0200

    DIO-FormacaoJavaDeveloper - classes, metodos, argumentos - Aprendendo Sintaxe Java > Anatomia das Classes 1

---
+++ DIO_FormacaoJavaDeveloper/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {
+    public static void main(String [] args){ //metodo main recebe parametro args
+        System.out.print ("Primeira classe com metodo executavel");
+    }
+}
