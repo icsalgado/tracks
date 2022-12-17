7eed1782ce981cccc31c4efd771ac1992c2380c2
icsalgado <icsalgado@icloud.com>
2022-12-17 21:11:55 -0200

    TW-FormacaoDesenvolvedorJava - cast para inteiro - Entendendo conceito de cast

--- TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
+++ TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
@@ -6,16 +6,28 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá mundo"); // quebra a linha
+		/*System.out.println("Olá mundo"); // quebra a linha
 		System.out.print("Olá de novo"); // não quebra a linha
 		System.out.print("Olá mais uma vez \n"); // quebra a linha com
 													// metacaractere pode usar
 													// também o \r no windows
+		
 		Scanner scanner = new Scanner(System.in); //instaciado scanner, isso por que ele não é global
 		
 		System.out.println("Informe seu nome");
-		String nome = scanner.nextLine();
+		String nome = scanner.nextLine();//faz a leitura da proxima linha
 		System.out.println(nome);
+		
+		scanner.close();//para encerrar da stresm o scanner*/
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe um numero");
+		int primeiroNumero = Integer.parseInt(scanner.nextLine());//casting para inteiro
+		
+		System.out.println(primeiroNumero);
+		
+		scanner.close();
 	}
 
 }
