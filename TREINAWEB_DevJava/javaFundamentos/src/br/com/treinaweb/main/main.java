1c36b2984f2aeeed492b36832dd7e288ba447912
icsalgado <icsalgado@icloud.com>
2022-12-17 21:33:23 -0200

    TW-FormacaoDesenvolvedorJava - usando next e nextLine - Entendendo Dif next e nextLine

--- TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
+++ TREINAWEB_DevJava/javaFundamentos/src/br/com/treinaweb/main/main.java
@@ -8,9 +8,7 @@ public class main {
 		// TODO Auto-generated method stub
 		/*System.out.println("Olá mundo"); // quebra a linha
 		System.out.print("Olá de novo"); // não quebra a linha
-		System.out.print("Olá mais uma vez \n"); // quebra a linha com
-													// metacaractere pode usar
-													// também o \r no windows
+		System.out.print("Olá mais uma vez \n"); // quebra a linha com metacaractere pode usar também o \r no windows
 		
 		Scanner scanner = new Scanner(System.in); //instaciado scanner, isso por que ele não é global
 		
@@ -25,12 +23,17 @@ public class main {
 		System.out.println("Informe um numero");
 		int primeiroNumero = Integer.parseInt(scanner.nextLine());//casting para inteiro
 		
-		System.out.println(primeiroNumero);
+		//System.out.println(primeiroNumero);
 		
 		System.out.println("Informe um numero");
-		int segundoNumero = scanner.nextInt();
+		int segundoNumero = scanner.nextInt();//retorna de fato um inteiro
 		
-		System.out.println(segundoNumero);
+		//System.out.println(segundoNumero);
+		
+		System.out.println("Informe a operacao");
+		char operacao = scanner.next().charAt(0);//.next.charAt vai capturar o caractere na posição indicada
+		
+		System.out.println(operacao);
 		
 		scanner.close();
 	}
