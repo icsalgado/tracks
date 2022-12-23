c25b2788dd4639d18b285e2039e42c2329d41315
icsalgado <icsalgado@icloud.com>
2022-12-23 17:59:51 -0300

    TW-DesenvolvedorPython - finalizada unidade 3 Primeiros passos com Python

--- TREINAWEB_DevPython/pyhtonFundamentos/main.py
+++ TREINAWEB_DevPython/pyhtonFundamentos/main.py
@@ -5,3 +5,11 @@ sobrenome = "Salgado"
 
 print("Olá", nome, sobrenome, idade)
 print(f"Olá {nome} {sobrenome} {idade}")
+
+nomeDoMeio = input("Informe seu nome do meio: ")
+
+print(f"Nome completo {nome} {nomeDoMeio} {sobrenome}")
+
+idade = int(input("Informe a idade atual: "))
+
+print(f"Nome completo {nome} {nomeDoMeio} {sobrenome}, {idade}")
