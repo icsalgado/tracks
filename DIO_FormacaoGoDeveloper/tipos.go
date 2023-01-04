47bb001267598d337bc80492b55fac191ae70dc8
icsalgado <icsalgado@icloud.com>
2023-01-04 04:23:16 -0300

    DIO-FormacaoGoDeveloper - Inferencia de tipo - Fundamentos de GO - Primeiros passos com GO

---
+++ DIO_FormacaoGoDeveloper/tipos.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("2 + 2 = ", 2+2)
+
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[2])
+	fmt.Println("Hello" + " " + "World")
+
+
+}
