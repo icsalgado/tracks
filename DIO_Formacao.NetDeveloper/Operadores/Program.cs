c1ef12efc5c9288b4c926f6a13897a61e2b59a2b
icsalgado <icsalgado@icloud.com>
2023-01-04 02:26:34 -0300

    DIO-FormacaoDotNetDeveloper - Operadores de atribuicao e casting > Tipos de operadores em Csharp - Fundamentos DotNet

---
+++ DIO_Formacao.NetDeveloper/Operadores/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+int a = 10;
+int b = 15;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+c++; //c + 1
+Console.WriteLine(c);
+
+c--; //c - 1
+Console.WriteLine(c);
+
+c += 5; // c + 5
+Console.WriteLine(c);
+
+c -= 5; //c - 5
+Console.WriteLine(c);
+
+int numero = Convert.ToInt32("5"); //casting
+int outroNumero = int.Parse("5"); //casting
+
+String letra = numero.ToString(); //casting
+
+Console.WriteLine(numero);
+Console.WriteLine(outroNumero);
+Console.WriteLine(letra);
+
+String valor = "15";
+int outroValor = 0;
+
+int.TryParse(valor, out outroValor); //tenta converter, se não der certo imprime o outroValor, se der certo armazena em outroValor
+
+Console.WriteLine(outroValor);
+
