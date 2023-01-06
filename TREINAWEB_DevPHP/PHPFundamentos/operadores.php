d9b4cb77c421b768a92a6ed4e17824c27713a188
icsalgado <icsalgado@icloud.com>
2023-01-06 03:22:12 -0300

    TW-FormaçaoDevPHP - 4 Operadores - PHP Fundamentos

---
+++ TREINAWEB_DevPHP/PHPFundamentos/operadores.php
@@ -0,0 +1,16 @@
+<?php
+
+$soma = 10 + 10;
+$subtracao = 10 - 10;
+$multiplicacao = 10 * 10;
+$divisao = 10 / 3;
+$modulo = 10 % 3;
+$exponencial = 10 ** 3; //so funciona a partir do php 5.6
+
+var_dump($soma);
+var_dump($subtracao);
+var_dump($multiplicacao);
+var_dump($divisao);
+var_dump($modulo);
+var_dump($exponencial);
+
