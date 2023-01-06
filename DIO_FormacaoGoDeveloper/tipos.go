f2e696fd4187df4d62c4afc28365444cce36f38b
icsalgado <icsalgado@icloud.com>
2023-01-06 02:00:27 -0300

    DIO-FormacaoGoDeveloper - Fundamentos de GO - Primeiros passos com GO

--- DIO_FormacaoGoDeveloper/tipos.go
+++ DIO_FormacaoGoDeveloper/tipos.go
@@ -8,5 +8,9 @@ func main(){
 	fmt.Println("Hello World"[2])
 	fmt.Println("Hello" + " " + "World")
 
+	var array[5] int //array de 5 inteiros
+	array[4] = 80 //posicao 4 recebe 80
+
+	fmt.Println(array)
 
 }
