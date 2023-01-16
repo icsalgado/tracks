86afb5f9806468855a832287d8980c5e12bfe123
icsalgado <icsalgado@icloud.com>
2023-01-16 03:33:59 -0300

    TW-FormacaoCsharp - 6 Conversao de dados 7 Operadores aritmeticos

---
+++ TREINAWEB_DevC#AspNet/C#Basico/CsharpBasico/BoxingVsUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoxingVsUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing - armazenar um value type em um reference type - passa da memoria Stack para a RIP
+            int inteiro1 = 2;
+            object objeto1 = inteiro1;
+
+            Console.WriteLine("Object = {0}", objeto1);
+
+            //Unboxing - vai precisar fazer o cast
+            int inteiro2 = (int)objeto1;
+
+            Console.WriteLine("inteiro2 = {0}", inteiro2);
+        }
+    }
+}
---
+++ TREINAWEB_DevC#AspNet/C#Basico/CsharpBasico/OperadoresAritmeticos/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperadoresAritmeticos
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            int resultado = numero1 + numero2;
+
+            txbResultado.Text = resultado.ToString();
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            int resultado = numero1 - numero2;
+
+            txbResultado.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            int resultado = numero1 * numero2;
+
+            txbResultado.Text = resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            int resultado = numero1 / numero2;
+
+            txbResultado.Text = resultado.ToString();
+        }
+    }
+}
