d209dc4868db2a6a9d60defe66758907a2802893
icsalgado <icsalgado@icloud.com>
2023-01-16 05:25:36 -0300

    TW-DesenvolvedorPython - 7 Estruturas de Repeticao - Python Fundamentos

---
+++ TREINAWEB_DevPython/pyhtonFundamentos/estruturas.py
@@ -0,0 +1,12 @@
+numero = 1
+
+while(numero < 10):
+    print(numero)
+    numero = numero+1
+else:
+    print("encerrado com sucesso")
+
+for numero in range(0, 21, 2):
+    print(numero)
+else:
+    print("Encerrado com sucesso")
