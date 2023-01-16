ffdca3aa3182cad54a5d60fd8ded233128e71a9f
icsalgado <icsalgado@icloud.com>
2023-01-16 00:18:47 -0300

    CAPGRMINI Start LP3 aula 4P

---
+++ CAPGEMINI_Start/LP3/Estruturas/src/main/java/Estruturas/Main.java
@@ -0,0 +1,50 @@
+
+package Estruturas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String aula  = "Estruturas";
+        System.out.println("Aulas de " + aula);// concatenação
+        
+        int codigo = 2;
+        
+        // estrutura de condição
+        if(codigo > 2){
+        
+        }else if(codigo < 2){
+        
+        }else {
+        
+        }
+        //Estrutura de condição
+        switch(codigo)
+        {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+                
+            default:
+    
+        }
+        //Estrutura de repetição
+        for(int i = 0; i < 10; i++){
+            
+        }
+        int j = 0;
+        for(;j < 10; j++){
+            
+        }
+        //Estrutura de repetição
+        while(codigo < 10){
+            
+            codigo++;
+        }
+        
+    }
+    
+}
