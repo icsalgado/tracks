ebbbdff739ab459a12d51d4363cc5a9dc1802ab9
icsalgado <icsalgado@icloud.com>
2023-01-18 23:47:05 -0300

    DIO-FormacaoJavaDeveloper - Finalizado Aprendendo sintaxe Java - Fundamentos da Plataforma

---
+++ DIO_FormacaoJavaDeveloper/Javadoc/src/App.java
@@ -0,0 +1,21 @@
+/**
+ * <h1>Documentação</h1>
+ * Aqui vai uma explicação qualquer da classe
+ * @author Iuri Salgado
+ * @version 1.0
+ * @since 18/01/2023
+ */
+
+
+public class App {
+
+    /**
+     * Primeira classe do programa
+     */
+    public static void main(String[] args) throws Exception {
+        /**
+         * metodo Básico do inicio
+         */
+        System.out.println("Hello, World!");
+    }
+}
---
+++ DIO_FormacaoJavaDeveloper/Javadoc/src/MinhaClass.java
@@ -0,0 +1,8 @@
+public class MinhaClass {
+    public static void main(String[] args) throws Exception {
+        /**
+         * metodo Básico do inicio
+         */
+        System.out.println("Hello, World!");
+    }
+}
