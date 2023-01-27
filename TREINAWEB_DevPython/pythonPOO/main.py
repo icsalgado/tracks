2fe34684e22f66aa252cae570eb9550c34d5f17a
icsalgado <icsalgado@icloud.com>
2023-01-27 01:54:50 -0300

    TW-DesenvolvedorPython - 1 Escopos e namespaces - Python POO

---
+++ TREINAWEB_DevPython/pythonPOO/main.py
@@ -0,0 +1,10 @@
+#metodos para importar bibliotecas
+
+from math import pow #importa um metodo
+from math import factorial, log #importa mais de um metodo
+from math import * #importa todos os metodos
+
+import math #vai precisar usar a biblioteca.metodo ex print(math.pow(2, 2))
+
+
+print(math.pow(2, 2))
